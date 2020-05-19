17b5fbf0b1ea0fcc735636058026c3e2001d0f66
Sungjae Lee <odobenuseKR@gmail.com>
2020-05-19 16:51:36 +0900

    Update instance.sh

--- instance.sh
+++ instance.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
+
 aws ec2 run-instances --image-id ami-abcd1234 --count 1 --instance-type m3.medium \
 --key-name my-key-pair --subnet-id subnet-abcd1234 --security-group-ids sg-abcd1234 \
 --user-data file://user_script.txt
 
+INSTANCE_ID=$(echo $START_LOG | jq -r '. | .Instances[0].InstanceId')
+echo $INSTANCE_ID
