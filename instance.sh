80157b917e312d22ff9a2b66c0ce4e9584067f3e
Sungjae Lee <odobenuseKR@gmail.com>
2020-05-19 21:05:54 +0900

    Update instance.sh

--- instance.sh
+++ instance.sh
@@ -8,10 +8,17 @@ INSTANCE_ID=$(echo $INSTANCE_INFO | jq -r '. | .Instances[0].InstanceId')
 INSTANCE_PUB_DNS=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID | jq -r '. | .Reservations[0].Instances[0].PublicDnsName')
 echo $INSTANCE_PUB_DNS
 
+# Start Settings
 sleep 30
 echo 'clone start'
-ssh -o "StrictHostKeyChecking no" -i awspwd.pem ubuntu@$INSTANCE_PUB_DNS 'git clone https://github.com/odobenuskr/Deep-Cloud.git;cd Deep-Cloud'
+ssh -o "StrictHostKeyChecking no" -i awspwd.pem ubuntu@$INSTANCE_PUB_DNS 'git clone https://github.com/odobenuskr/Deep-Cloud.git'
 echo 'setting start'
-ssh -i awspwd.pem ubuntu@$INSTANCE_PUB_DNS 'bash ./settings.sh'
+ssh -i awspwd.pem ubuntu@$INSTANCE_PUB_DNS 'cd Deep-Cloud;sudo bash ./settings.sh'
+
+# Run Experiments
+sleep 10
 echo 'run start'
-ssh -i awspwd.pem ubuntu@$INSTANCE_PUB_DNS 'bash ./run.sh'
+ssh -i awspwd.pem ubuntu@$INSTANCE_PUB_DNS 'cd Deep-Cloud;sudo bash ./run.sh'
+
+# Run Tensorboard backgroound
+sleep 10
