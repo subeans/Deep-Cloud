21a1210c0b43be36c1f5dd40daae2a60e0f7138b
Sungjae Lee <odobenuseKR@gmail.com>
2020-05-19 20:10:49 +0900

    Update instance.sh

--- instance.sh
+++ instance.sh
@@ -4,5 +4,15 @@ aws ec2 run-instances --image-id ami-abcd1234 --count 1 --instance-type m3.mediu
 --key-name my-key-pair --subnet-id subnet-abcd1234 --security-group-ids sg-abcd1234 \
 --user-data file://user_script.txt
 
-INSTANCE_ID=$(echo $START_LOG | jq -r '. | .Instances[0].InstanceId')
-echo $INSTANCE_ID
+sleep 30
+INSTANCE_ID=$(echo $INSTANCE_INFO | jq -r '. | .Instances[0].InstanceId')
+INSTANCE_PUB_DNS=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID | jq -r '. | .Reservations[0].Instances[0].PublicDnsName')
+echo $INSTANCE_PUB_DNS
+
+sleep 30
+echo 'clone start'
+ssh -o "StrictHostKeyChecking no" -i awspwd.pem uvuntu@$INSTANCE_PUB_DNS 'git clone https://github.com/odobenuskr/Deep-Cloud.git;cd Deep-Cloud'
+echo 'setting start'
+ssh -i awspwd.pem uvuntu@$INSTANCE_PUB_DNS 'bash ./settings.sh'
+echo 'run start'
+ssh -i awspwd.pem uvuntu@$INSTANCE_PUB_DNS 'bash ./run.sh'
