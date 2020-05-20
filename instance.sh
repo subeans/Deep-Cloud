03da222532c2f3f7b26ce29cdce8405b481b72e1
Sungjae Lee <odobenuseKR@gmail.com>
2020-05-20 11:12:33 +0900

    Update instance.sh

--- instance.sh
+++ instance.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 INSTANCE_TYPE=$1
-INSTANCE_INFO=$(aws ec2 run-instances --image-id ami-abcd1234 --count 1 --instance-type $INSTANCE_TYPE \
+LAUNCH_INFO=$(aws ec2 run-instances --image-id ami-abcd1234 --count 1 --instance-type $INSTANCE_TYPE \
 --key-name my-key-pair --subnet-id subnet-abcd1234 --security-group-ids sg-abcd1234)
 
 sleep 30
-INSTANCE_ID=$(echo $INSTANCE_INFO | jq -r '. | .Instances[0].InstanceId')
+INSTANCE_ID=$(echo $LAUNCH_INFO | jq -r '. | .Instances[0].InstanceId')
 INSTANCE_PUB_DNS=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID | jq -r '. | .Reservations[0].Instances[0].PublicDnsName')
 echo $INSTANCE_PUB_DNS
-
 # Start Settings
 sleep 30
 echo 'clone start'
@@ -19,7 +18,13 @@ ssh -i awspwd.pem ubuntu@$INSTANCE_PUB_DNS 'cd Deep-Cloud;sudo bash ./settings.s
 # Run Experiments
 sleep 10
 echo 'run start'
-ssh -i awspwd.pem ubuntu@$INSTANCE_PUB_DNS 'cd Deep-Cloud;sudo bash ./run.sh'
+BASE_COMMAND="cd Deep-Cloud;sudo bash ./run.sh "
+RUN_COMMAND="$BASE_COMMAND$INSTANCE_TYPE"
+ssh -i awspwd.pem ubuntu@$INSTANCE_PUB_DNS $RUN_COMMAND
 
 # Run Tensorboard backgroound
 sleep 10
+
+# Terminate Instance
+TERMINATE_INFO=$(aws ec2 terminate-instances --instance-ids $INSTANCE_ID)
+echo $TERMINATE_INFO
