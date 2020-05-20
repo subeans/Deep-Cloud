a60a2bd04452dbed154285869d08d633f95ad103
Sungjae Lee <odobenuseKR@gmail.com>
2020-05-20 16:11:17 +0900

    Update tensorboard_result.sh

--- tensorboard_result.sh
+++ tensorboard_result.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 tensorboard --logdir=./logs &
-sleep 10
+sleep 5
 
+INSTANCE_TYPE=$1
 NAME_LIST=$(find ./logs -name *.trace.json.gz)
 
 TB_URL1="http://localhost:6006/data/plugin/profile/data?run="
@@ -11,13 +12,13 @@ TB_URL7="&tqx=out:csv;"
 
 for NAME in $NAME_LIST
 do
-        CSV=".csv"
-        FILENAME="$NAME$CSV"
         TB_URL2=$(echo $NAME | cut -d "/" -f 3)
         TB_URL4=$(echo $NAME | cut -d "/" -f 7)
         TB_URL6=$(echo $NAME | cut -d "/" -f 8 | cut -d "." -f 1)
         TB_URL="$TB_URL1$TB_URL2$TB_URL3$TB_URL4$TB_URL5$TB_URL6$TB_URL7"
         echo $TB_URL
+        FILENAME="./tensorstats/$TB_URL2$TB_URL4.csv"
         curl -o $FILENAME $TB_URL
-        sleep 5
+        sleep 1
 done
+
