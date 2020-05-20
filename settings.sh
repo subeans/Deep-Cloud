7e17094ba739c3cf38b013d77f10930a2d57afd5
Sungjae Lee <odobenuseKR@gmail.com>
2020-05-20 20:36:43 +0900

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
-
 # Based on AWS DeepLearning AMI with Ubuntu 18.04
-
-# Set the CUDA Version as 10.1
-# TensorFlow Profiler and CUPTI Doesn't Work on CUDA 10.2
-# At 2020. 05. 03
-sudo rm /usr/local/cuda
-sudo ln -s /usr/local/cuda-10.1 /usr/local/cuda
-
 # Upgrade pip3 and install tensorflow, tensorboard, dataset packages
-sudo pip3 install --upgrade pip
-sudo pip3 install tf-nightly 
-sudo pip3 install tb-nightly 
-sudo pip3 install tensorboard_plugin_profile
-sudo pip3 install tensorflow_datasets
+pip3 install --upgrade pip
+pip3 install tf-nightly 
+pip3 install tb-nightly 
+pip3 install tensorboard_plugin_profile
+pip3 install tensorflow_datasets
