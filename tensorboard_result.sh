9503210b1568f948f53ad05bf0d316c6217cdf30
Sungjae Lee <odobenuseKR@gmail.com>
2020-05-20 14:07:21 +0900

    Update tensorboard_result.sh

--- tensorboard_result.sh
+++ tensorboard_result.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-tensorboard --logdir=./logs
+tensorboard --logdir=./logs-16 &
+
+NAME_LIST=$(find ./ -name *.trace.json.gz)
 
 TB_URL1="http://localhost:6006/data/plugin/profile/data?run="
-TB_URL2="20200514-105804"
 TB_URL3="/train/"
-TB_URL4="2020_05_14_10_58_22"
-TB_URL5="&tag=tensorflow_stats&host=ip-172-31-20-125&tqx=out:csv;"
-TB_URL="$TB_URL1$TB_URL2$TB_URL3$TB_URL4$TB_URL5"
+TB_URL5="&tag=tensorflow_stats&host=ip-"
+TB_URL7="&tqx=out:csv;"
 
-for FILENAME in 
+for NAME in $NAME_LIST
 do
-        curl -o $FILENAME $TB_URL
+        FILENAME="$NAME.csv"
+        TB_URL2=$(echo $NAME | cut -d "/" -f 3)
+        TB_URL4=$(echo $NAME | cut -d "/" -f 7)
+        TB_URL6=$(echo $NAME | cut -d "/" -f 8 | awk -F '[-.]' '{print $2"-"$3"-"$4"-"$5}')
+        TB_URL="$TB_URL1$TB_URL2$TB_URL3$TB_URL4$TB_URL5$TB_URL6$TB_URL7"
+        echo TB_URL
+#        curl -o $FILENAME $TB_URL
 done
