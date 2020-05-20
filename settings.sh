88728616316a22a1c6f5ef8fe0a597759583b9e0
Sungjae Lee <odobenuseKR@gmail.com>
2020-05-20 19:13:05 +0900

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -5,12 +5,12 @@
 # Set the CUDA Version as 10.1
 # TensorFlow Profiler and CUPTI Doesn't Work on CUDA 10.2
 # At 2020. 05. 03
-rm /usr/local/cuda
-ln -s /usr/local/cuda-10.1 /usr/local/cuda
+sudo rm /usr/local/cuda
+sudo ln -s /usr/local/cuda-10.1 /usr/local/cuda
 
 # Upgrade pip3 and install tensorflow, tensorboard, dataset packages
-pip3 install --upgrade pip
-pip3 install tf-nightly 
-pip3 install tb-nightly 
-pip3 install tensorboard_plugin_profile
-pip3 install tensorflow_datasets
+sudo pip3 install --upgrade pip
+sudo pip3 install tf-nightly 
+sudo pip3 install tb-nightly 
+sudo pip3 install tensorboard_plugin_profile
+sudo pip3 install tensorflow_datasets
