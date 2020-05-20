fdb44a4df77fdd1f62d54672c3344d23ba4ae751
Sungjae Lee <odobenuseKR@gmail.com>
2020-05-20 10:31:00 +0900

    Update instance.sh

--- instance.sh
+++ instance.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-INSTANCE_INFO=$(aws ec2 run-instances --image-id ami-abcd1234 --count 1 --instance-type m3.medium \
+INSTANCE_TYPE=$1
+INSTANCE_INFO=$(aws ec2 run-instances --image-id ami-abcd1234 --count 1 --instance-type $INSTANCE_TYPE \
 --key-name my-key-pair --subnet-id subnet-abcd1234 --security-group-ids sg-abcd1234)
 
 sleep 30
