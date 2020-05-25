6695f3448ab18127efdf29082f1e39d04a83e0da
Sungjae Lee <odobenuseKR@gmail.com>
2020-05-25 15:40:26 +0900

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # Based on AWS DeepLearning AMI with Ubuntu 18.04
+
+rm /usr/local/cuda
+ln -s /usr/local/cuda-10.1 /usr/local/cuda
+
 # Upgrade pip3 and install tensorflow, tensorboard, dataset packages
 pip3 install --upgrade pip
 pip3 install tf-nightly 
