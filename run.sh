bd32d53a4bb133e432de924627c4d2ccf293ac5f
Sungjae Lee <odobenuseKR@gmail.com>
2020-05-25 13:54:53 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 INSTANCE_TYPE=$1
 
 # Get profile result at half of 2epoch
-for BATCH_SIZE in 16 32 64 128 256 512
+for BATCH_SIZE in 64 # 16 32 64 128 256 512
 do
         DATA_SIZE=60000
         BATCH_IDX_START=$((($DATA_SIZE/$BATCH_SIZE)*3/2))
