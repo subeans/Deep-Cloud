c02512719b33cd6fbbc5482d2400116d9ac74dce
Sungjae Lee <odobenuseKR@gmail.com>
2020-05-26 10:37:54 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,13 +2,22 @@
 # INSTANCE_TYPE=$1
 
 # Get profile result at half of 2epoch
-for BATCH_SIZE in 64 # 16 32 64 128 256 512
-do
-        DATA_SIZE=60000
-        BATCH_IDX_START=$((($DATA_SIZE/$BATCH_SIZE)*3/2))
-        BATCH_IDX_END=$(($BATCH_IDX_START+1))
-        echo $BATCH_IDX_START
-        echo $BATCH_IDX_END
-        python3.6 profile.py --batch_size $BATCH_SIZE \
-        --prof_start_batch $BATCH_IDX_START --prof_end_batch $BATCH_IDX_END
-done
+sudo -i -u root bash << EOF
+python3.6 /home/ubuntu/Deep-Cloud/profile.py --batch_size 16 --prof_start_batch 5625 --prof_end_batch 5626
+sleep 3
+
+python3.6 /home/ubuntu/Deep-Cloud/profile.py --batch_size 32 --prof_start_batch 2812 --prof_end_batch 2813
+sleep 3
+
+python3.6 /home/ubuntu/Deep-Cloud/profile.py --batch_size 64 --prof_start_batch 1405 --prof_end_batch 1406
+sleep 3
+
+python3.6 /home/ubuntu/Deep-Cloud/profile.py --batch_size 128 --prof_start_batch 702 --prof_end_batch 703
+sleep 3
+
+python3.6 /home/ubuntu/Deep-Cloud/profile.py --batch_size 256 --prof_start_batch 351 --prof_end_batch 352
+sleep 3
+
+python3.6 /home/ubuntu/Deep-Cloud/profile.py --batch_size 512 --prof_start_batch 175 --prof_end_batch 176
+sleep 3
+EOF
