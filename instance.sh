b32f90a71ec94afd7c1a2d2776c05da87d7bbea7
Sungjae Lee <odobenuseKR@gmail.com>
2020-05-26 11:05:50 +0900

    Update instance.sh

--- instance.sh
+++ instance.sh
@@ -4,35 +4,38 @@ INSTANCE_TYPE=$1
 LAUNCH_INFO=$(aws ec2 run-instances --image-id ami-abcd1234 --count 1 --instance-type $INSTANCE_TYPE \
 --key-name my-key-pair --subnet-id subnet-abcd1234 --security-group-ids sg-abcd1234)
 
-sleep 30
+# Instance ID and Public DNS Parsing
+sleep 20
 INSTANCE_ID=$(echo $LAUNCH_INFO | jq -r '. | .Instances[0].InstanceId')
 INSTANCE_PUB_DNS=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID | jq -r '. | .Reservations[0].Instances[0].PublicDnsName')
 echo $INSTANCE_PUB_DNS
-# Start Settings
-sleep 30
+
+# Setting for Deep Learning
+sleep 10
 echo 'clone start'
 ssh -o "StrictHostKeyChecking no" -i awspwd.pem ubuntu@$INSTANCE_PUB_DNS 'git clone https://github.com/odobenuskr/Deep-Cloud.git'
 echo 'setting start'
-ssh -i awspwd.pem ubuntu@$INSTANCE_PUB_DNS 'cd Deep-Cloud;sudo bash ./settings.sh'
+ssh -i awspwd.pem -t ubuntu@$INSTANCE_PUB_DNS 'cd /home/ubuntu/Deep-Cloud/;sudo bash ./settings.sh'
 
 # Run Experiments
 sleep 10
 echo 'run start'
-BASE_COMMAND="cd Deep-Cloud;sudo bash ./run.sh "
+BASE_COMMAND="cd /home/ubuntu/Deep-Cloud/;sudo bash ./run.sh "
 RUN_COMMAND="$BASE_COMMAND$INSTANCE_TYPE"
-ssh -i awspwd.pem ubuntu@$INSTANCE_PUB_DNS $RUN_COMMAND
+ssh -i awspwd.pem -t ubuntu@$INSTANCE_PUB_DNS $RUN_COMMAND
 
-# Run Experiments
+# Run Tensorboard backgroound
 sleep 10
-echo 'run start'
-BASE_COMMAND="cd Deep-Cloud;sudo bash ./run.sh "
-RUN_COMMAND="$BASE_COMMAND$INSTANCE_TYPE"
-ssh -i awspwd.pem ubuntu@$INSTANCE_PUB_DNS $RUN_COMMAND
+BASE_COMMAND2="cd /home/ubuntu/Deep-Cloud/;sudo bash ./tensorboard_result.sh "
+RUN_COMMAND2="$BASE_COMMAND2$INSTANCE_TYPE"
+ssh -i awspwd.pem -t ubuntu@$INSTANCE_PUB_DNS $RUN_COMMAND2
 
-# Get Tensorboard Results
+# Get csv files from instance
 sleep 10
-ssh -i awspwd.pem ubuntu@$INSTANCE_PUB_DNS "cd Deep-Cloud;sudo bash ./tensorboard_result.sh"
-
+scp -i awspwd.pem \
+ubuntu@$INSTANCE_PUB_DNS:~/Deep-Cloud/tensorstats/* ./
+ 
 # Terminate Instance
+sleep 10
 TERMINATE_INFO=$(aws ec2 terminate-instances --instance-ids $INSTANCE_ID)
 echo $TERMINATE_INFO
