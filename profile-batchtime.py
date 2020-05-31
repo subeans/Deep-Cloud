a8851da89b9f49a0476180c11ea25a84b5995536
Sungjae Lee <odobenuseKR@gmail.com>
2020-05-31 14:18:07 +0900

    Update profile-batchtime.py

--- profile-batchtime.py
+++ profile-batchtime.py
@@ -79,7 +79,7 @@ class BatchTimeCallback(tf.keras.callbacks.Callback):
         self.all_times = []
 
     def on_train_end(self, logs=None):
-	time_filename = "./tensorstats/times-" + str(args.batch_size) + "-" + datetime.now().strftime("%Y%m%d-%H%M%S") + ".pickle"
+        time_filename = "./tensorstats/times-" + str(args.batch_size) + "-" + datetime.now().strftime("%Y%m%d-%H%M%S") + ".pickle"
         time_file = open(time_filename, 'ab')
         pickle.dump(self.all_times, time_file)
         time_file.close()
