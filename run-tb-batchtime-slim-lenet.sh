266642166a2605f5cf545c7a4fb7b69172976cab
Sungjae Lee <odobenuseKR@gmail.com>
2020-06-02 12:22:10 +0900

    Create run-tb-batchtime-slim-lenet.sh

---
+++ run-tb-batchtime-slim-lenet.sh
@@ -0,0 +1,24 @@
+# Get Instance Type
+# INSTANCE_TYPE=$1
+
+# Get profile result at half of 2epoch
+sudo -i -u root bash << EOF
+python3.6 /home/ubuntu/Deep-Cloud/profile-slim-lenet.py --batch_size 16 --prof_start_batch 5625 --prof_end_batch 5626
+sleep 3
+
+python3.6 /home/ubuntu/Deep-Cloud/profile-slim-lenet.py --batch_size 32 --prof_start_batch 2812 --prof_end_batch 2813
+sleep 3
+
+python3.6 /home/ubuntu/Deep-Cloud/profile-slim-lenet.py --batch_size 64 --prof_start_batch 1405 --prof_end_batch 1406
+sleep 3
+
+python3.6 /home/ubuntu/Deep-Cloud/profile-slim-lenet.py --batch_size 128 --prof_start_batch 702 --prof_end_batch 703
+sleep 3
+
+python3.6 /home/ubuntu/Deep-Cloud/profile-slim-lenet.py --batch_size 256 --prof_start_batch 351 --prof_end_batch 352
+sleep 3
+
+python3.6 /home/ubuntu/Deep-Cloud/profile-slim-lenet.py --batch_size 512 --prof_start_batch 175 --prof_end_batch 176
+sleep 3
+
+EOF
