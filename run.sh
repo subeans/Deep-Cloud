d09227c944f412f3376623e5a9cfb6847e1c05e1
Sungjae Lee <odobenuseKR@gmail.com>
2020-06-08 23:30:07 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,22 +7,22 @@ sudo -i -u root bash << EOF
 python3.6 /home/ubuntu/Deep-Cloud/test-profile.py --batch_size 16 --prof_start_batch 5625 --prof_end_batch 5626
 sleep 3
 
-python3.6 /home/ubuntu/Deep-Cloud/$JOB_NAME --batch_size 16 --prof_start_batch 5625 --prof_end_batch 5626
+python3.6 /home/ubuntu/Deep-Cloud/job/$JOB_NAME --batch_size 16 --prof_start_batch 5625 --prof_end_batch 5626
 sleep 3
 
-python3.6 /home/ubuntu/Deep-Cloud/$JOB_NAME --batch_size 32 --prof_start_batch 2812 --prof_end_batch 2813
+python3.6 /home/ubuntu/Deep-Cloud/job/$JOB_NAME --batch_size 32 --prof_start_batch 2812 --prof_end_batch 2813
 sleep 3
 
-python3.6 /home/ubuntu/Deep-Cloud/$JOB_NAME --batch_size 64 --prof_start_batch 1405 --prof_end_batch 1406
+python3.6 /home/ubuntu/Deep-Cloud/job/$JOB_NAME --batch_size 64 --prof_start_batch 1405 --prof_end_batch 1406
 sleep 3
 
-python3.6 /home/ubuntu/Deep-Cloud/$JOB_NAME --batch_size 128 --prof_start_batch 702 --prof_end_batch 703
+python3.6 /home/ubuntu/Deep-Cloud/job/$JOB_NAME --batch_size 128 --prof_start_batch 702 --prof_end_batch 703
 sleep 3
 
-python3.6 /home/ubuntu/Deep-Cloud/$JOB_NAME --batch_size 256 --prof_start_batch 351 --prof_end_batch 352
+python3.6 /home/ubuntu/Deep-Cloud/job/$JOB_NAME --batch_size 256 --prof_start_batch 351 --prof_end_batch 352
 sleep 3
 
-python3.6 /home/ubuntu/Deep-Cloud/$JOB_NAME --batch_size 512 --prof_start_batch 175 --prof_end_batch 176
+python3.6 /home/ubuntu/Deep-Cloud/job/$JOB_NAME --batch_size 512 --prof_start_batch 175 --prof_end_batch 176
 sleep 3
 
 EOF
