6ec6fc1dd5c63ddc5eb69d82cb37c765d2fe4fb9
Sungjae Lee <odobenuseKR@gmail.com>
2020-06-08 23:53:25 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ JOB_NAME=$1
 
 # Get profile result at half of 2epoch
 sudo -i -u root bash << EOF
-python3.6 /home/ubuntu/Deep-Cloud/test-profile.py --batch_size 16 --prof_start_batch 5625 --prof_end_batch 5626
+python3.6 /home/ubuntu/Deep-Cloud/job/test-profile.py --batch_size 16 --prof_start_batch 5625 --prof_end_batch 5626
 sleep 3
 
 python3.6 /home/ubuntu/Deep-Cloud/job/$JOB_NAME --batch_size 16 --prof_start_batch 5625 --prof_end_batch 5626
