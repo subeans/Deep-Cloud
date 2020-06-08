f2f8149e8d30f0c88e885811f356cbae8441778f
Sungjae Lee <odobenuseKR@gmail.com>
2020-06-09 00:00:25 +0900

    Update cifar10-lenet5.py

--- job/cifar10-lenet5.py
+++ job/cifar10-lenet5.py
@@ -41,11 +41,11 @@ img_rows, img_cols = 32, 32
 if tf.keras.backend.image_data_format() == 'channels_first':
     x_train = x_train.reshape(x_train.shape[0], 3, img_rows, img_cols)
     x_test = x_test.reshape(x_test.shape[0], 3, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
+    input_shape = (3, img_rows, img_cols)
 else:
     x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 3)
     x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 3)
-    input_shape = (img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 3)
 
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
