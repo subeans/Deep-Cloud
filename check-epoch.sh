6695f6c3649cd1bf642c4bc4c25eed6da0ab0b66
Sungjae Lee <odobenuseKR@gmail.com>
2020-06-11 14:52:25 +0900

    Create check-epoch.sh

---
+++ check-epoch.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+# Get Instance Type
+JOB_NAME=$1
+
+# Get profile result at half of 2epoch
+sudo -i -u root bash << EOF
+
+python3.6 /home/ubuntu/Deep-Cloud/job/$JOB_NAME --batch_size 128 --prof_start_batch 702 --prof_end_batch 703
+sleep 3
+
+EOF
