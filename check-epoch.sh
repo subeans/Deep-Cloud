203f4144db9a25375937720be65c0352a1317687
Sungjae Lee <odobenuseKR@gmail.com>
2020-06-11 15:08:31 +0900

    Update check-epoch.sh

--- check-epoch.sh
+++ check-epoch.sh
@@ -3,7 +3,7 @@
 JOB_NAME=$1
 
 # Get profile result at half of 2epoch
-sudo -i -u root bash << EOF
+sudo time -i -u root bash << EOF
 
 python3.6 /home/ubuntu/Deep-Cloud/job/$JOB_NAME --batch_size 128 --prof_start_batch 702 --prof_end_batch 703
 sleep 3
