3fe9a860081490bc7e3b2cb8f87d31b61402c289
Sungjae Lee <odobenuseKR@gmail.com>
2020-06-30 20:03:23 +0900

    Update and rename mnist-vgg16small.py to mnist-vggsmall.py

--- job/mnist-vggsmall.py
+++ job/mnist-vggsmall.py
@@ -62,32 +62,16 @@ y_test = tf.keras.utils.to_categorical(y_test, num_classes)
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
 model.add(tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu'))
-model.add(tf.keras.layers.MaxPooling2D((3, 3), strides=(2, 2)))
 
 model.add(tf.keras.layers.Conv2D(128, (3, 3), activation='relu'))
 model.add(tf.keras.layers.Conv2D(128, (3, 3), activation='relu'))
-model.add(tf.keras.layers.MaxPooling2D((3, 3), strides=(2, 2)))
 
 model.add(tf.keras.layers.Conv2D(256, (3, 3), activation='relu'))
 model.add(tf.keras.layers.Conv2D(256, (3, 3), activation='relu'))
-model.add(tf.keras.layers.Conv2D(256, (3, 3), activation='relu'))
-model.add(tf.keras.layers.MaxPooling2D((3, 3), strides=(2, 2)))
-
-# model.add(tf.keras.layers.Conv2D(512, (3, 3), activation='relu'))
-# model.add(tf.keras.layers.Conv2D(512, (3, 3), activation='relu'))
-# model.add(tf.keras.layers.Conv2D(512, (3, 3), activation='relu'))
-# model.add(tf.keras.layers.MaxPooling2D())
-
-# model.add(tf.keras.layers.Conv2D(512, (3, 3), activation='relu'))
-# model.add(tf.keras.layers.Conv2D(512, (3, 3), activation='relu'))
-# model.add(tf.keras.layers.Conv2D(512, (3, 3), activation='relu'))
-# model.add(tf.keras.layers.MaxPooling2D())
 
 model.add(tf.kears.layers.Flatten())
-# model.add(tf.keras.layers.Conv2D(4096, (7, 7), activation='relu'))
 model.add(tf.keras.layers.Dense(1024, activation='relu'))
 model.add(tf.keras.layers.Dropout(0.5))
-# model.add(tf.keras.layers.Dense(4096, activation='relu'))
 model.add(tf.keras.layers.Dense(1024, activation='relu'))
 model.add(tf.keras.layers.Dropout(0.5))
 
